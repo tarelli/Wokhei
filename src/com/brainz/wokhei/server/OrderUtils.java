383c23a2c6530ef8206afdc2543e1626dd67053c
tarelli <matteocantarelli@Macintosh-2.local>
2009-05-24 19:48:23 +0100

    Added Order Utils

---
+++ src/com/brainz/wokhei/server/OrderUtils.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.brainz.wokhei.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.brainz.wokhei.Order;
+import com.brainz.wokhei.shared.OrderDTO;
+
+/**
+ * @author matteocantarelli
+ *
+ */
+public class OrderUtils {
+
+	/**
+	 * @param order
+	 * @return
+	 */
+	public static OrderDTO getOrderDTO(Order order) 
+	{
+		String[] strArr = new String[order.getTags().size()];
+		System.arraycopy(order.getTags().toArray(), 0, strArr, 0, order.getTags().size());
+		return new OrderDTO(order.getId(),order.getText(),strArr,order.getDate(),order.getStatus(),order.getColour(), order.getCustomer().getEmail());
+	}
+
+	/**
+	 * @param orders
+	 * @return
+	 */
+	public static List<OrderDTO> getOrderDTOList(List<Order> orders)
+	{
+		List<OrderDTO> ordersDTO=new ArrayList<OrderDTO>();
+
+		if(orders != null)
+		{
+			for(Order o:orders)
+			{
+				ordersDTO.add(getOrderDTO(o));
+			}
+		}
+
+		return ordersDTO;
+	}
+
+}
